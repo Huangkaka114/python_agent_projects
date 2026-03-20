3ec6be40da12d8ff41aa6f850a859ecee426947a
黄卡卡 <3294141452@qq.com>
2026-03-20 17:10:17 +0800

    更新前端页面

---
+++ app/app.py
@@ -0,0 +1,68 @@
+import os
+import time
+
+import streamlit as st
+from chat_core import create_agent
+from dotenv import load_dotenv
+from langchain_core.chat_history import InMemoryChatMessageHistory
+from langchain_core.runnables import RunnableWithMessageHistory
+
+st.set_page_config(page_title="Chat Agent", page_icon="🤖")
+st.title("🤖 独立对话智能体")
+
+# 环境配置
+load_dotenv()
+api_key = os.getenv("API_KEY")
+base_url = os.getenv("BASE_URL")
+model = "qwen-turbo"
+
+if "chat_history" not in st.session_state:
+    st.session_state.chat_history = InMemoryChatMessageHistory()
+
+if "agent" not in st.session_state:
+    executor = create_agent(api_key, base_url, model)
+
+
+    agent_with_history = RunnableWithMessageHistory(executor, get_session_history=lambda
+        session_id: st.session_state.chat_history,
+                                                    input_messages_key="input", history_messages_key="history")
+    st.session_state.agent = agent_with_history
+
+# 消息历史
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+# 把所有聊天记录一条一条拿出来，每一条都放进对应的气泡里，然后把文字显示出来
+for msg in st.session_state.messages:
+    with st.chat_message(msg["role"]):
+        st.markdown(msg["content"], unsafe_allow_html=True)
+
+# 对话输入
+user_input = st.chat_input("请输入问题...")
+if user_input:
+    st.session_state.messages.append({"role": "user", "content": user_input})
+    with st.chat_message("user"):
+        st.markdown(user_input)
+
+    with st.chat_message("assistant"):
+        try:
+            with st.spinner("🧠 AI 思考中..."):
+                response = st.session_state.agent.invoke({"input": user_input},
+                                                         config={"configurable": {"session_id": "huangkaka"}})
+                full_response = response["output"]
+            placeholder = st.empty()
+
+            # 模拟流式（所有模型都稳定）
+            temp = ""
+            for char in full_response:
+                temp += char
+                placeholder.markdown(temp + "▌")
+                time.sleep(0.01)
+
+            placeholder.markdown(full_response)
+
+        except Exception :
+            full_response = "出现错误，请换个问题重试。"
+            st.error(full_response)
+
+    st.session_state.messages.append({"role": "assistant", "content": full_response})
---
+++ app/chat_core.py
@@ -0,0 +1,54 @@
+from langchain_classic.agents import create_react_agent, AgentExecutor
+from langchain_community.tools import DuckDuckGoSearchRun
+from langchain_core.prompts import PromptTemplate
+from langchain_openai import ChatOpenAI
+from numpy.f2py.crackfortran import verbose
+
+from sympy.physics.units import temperature
+
+
+def create_agent(api_key: str, base_url: str, model: str):
+    llm = ChatOpenAI(
+        api_key=api_key,
+        base_url=base_url,
+        model=model,
+        temperature=0.1
+    )
+    # 联网搜索
+    search = DuckDuckGoSearchRun()
+    tools = [search]
+
+    prompt = PromptTemplate.from_template("""
+        # 任务
+        你是一个严谨、可靠的智能助手，请严格按照规则回答用户问题。
+        
+        # 可用工具zhe
+        {tools}
+        
+        # 必须严格遵守的输出格式
+        Question: {input}
+        Thought: 分析是否需要使用工具，以及需要做什么
+        Action: 仅在需要工具时填写，不需要则**整行省略**
+        Action Input: 仅在有Action时填写，不需要则**整行省略**
+        Observation: 仅在有工具返回时填写，不需要则**整行省略**
+        Thought: 我已经可以给出最终答案
+        Final Answer: 用中文清晰、完整地回答用户
+        
+        # 核心规则（违反则输出错误）
+        1. 凡是**问候、闲聊、询问对话历史、询问上文、询问之前问题、询问记忆内容**，
+           → **绝对不允许使用任何工具**，直接给出 Final Answer。
+        2. 只有需要**实时信息、新闻、数据、外部知识**时，才允许使用搜索工具。
+        3. 不需要工具时，**严禁出现 Action / Action Input / Observation** 任何一行。
+        4. 回答必须使用中文，简洁、准确、不编造。
+        5. 若问题可从对话历史直接得出，禁止调用工具。
+        
+        # 对话历史
+        {history}
+        
+        # 开始
+        Question: {input}
+        Thought:{agent_scratchpad}""")
+
+    agent = create_react_agent(llm, tools, prompt)
+    executor = AgentExecutor(agent=agent, tools=tools, verbose=True, handle_parsing_errors=True,max_iterations=3,early_stopping_method="generate")
+    return executor
